926083545bc694dc1656cbe03fafec19eaf4aaf5
kroppian <iankropp@gmail.com>
2014-06-11 09:05:31 -0400

    Fixed the bug that the menu would disapear if you moused through the white space between the drop point and the menu

--- hoverMenus.js
+++ hoverMenus.js
@@ -2,6 +2,9 @@
 
 $(document).ready(function(){
 
+  $("ul :first-child").removeClass("dropdown-menu-item");
+  $("ul :first-child").addClass("dropdown-menu-item-top");
+
   $("ul").hide();
 
   function dropPointMouseIn() {
@@ -14,17 +17,17 @@ $(document).ready(function(){
 
   function listMouseIn() {
       $(this).css("background-color", "#BABABA");
-      $(this).children("ul").show()
+      //$(this).children("ul").show()
   }
 
   function listMouseOut() {
       $(this).css("background-color", "#E3E3E3");
-      $(this).children("ul").hide();
+      //$(this).children("ul").hide();
   }
 
   $(".drop-point").hover(dropPointMouseIn, dropPointMouseOut);
 
-  $(".dropdown-menu-item").hover(listMouseIn, listMouseOut);
+  $(".dropdown-menu-item,.dropdown-menu-item-top").hover(listMouseIn, listMouseOut);
 
 });
 
